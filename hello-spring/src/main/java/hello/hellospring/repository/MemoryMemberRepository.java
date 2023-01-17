6b596a188bf2e03e3902d02d0296d963a1dbd4c7
min0731 <ypd04208@gmail.com>
2023-01-17 16:51:49 +0900

    Java_Spring_01_17_intellij_idea

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -17,7 +17,7 @@ import java.util.*;
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long,Member> store = new HashMap<>();
-    private static long sequence = 0L; // 0,1,2.. 키값을 생성
+    private static long sequence = 0L; // 0,1,2.. 키값을 생성!
 
     @Override
     public Member save(Member member) {
